2ee5cae63c993aaf1a2fb1dc27e2708b625ae824
sofiagranlof <sofia.granlof@gmail.com>
2016-11-25 22:28:05 +0100

    Create api.js

---
+++ javascripts/api.js
@@ -0,0 +1,46 @@
+function APIConnect() {
+    var     baseURL = 'http://pub.jamaica-inn.net/fpdb/api.php',
+            username = '',
+            password = '';
+    
+    function constructURL(params) { 
+        var url = baseURL + '?' + 'username=' + username + '&' + 'password=' + password;
+        for(var key in params){
+            url = url + '&' + key + '=' + params[key] 
+        }
+        return url;
+    }
+    
+    function request(url, callback) { 
+        var xhr = new XMLHttpRequest();
+        
+        
+        xhr.onreadystatechange = function(){
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 200){
+                callback(this.responseText);
+            }
+        };
+        xhr.open('GET',url);
+        xhr.send();
+        
+    }
+    
+    this.setUser = function(un, pw) {
+        username = un;
+        password = pw;
+    };
+    
+    this.fetchUsers = function(callback) {
+        var url = constructURL({action: 'user_get_all'})
+        request(url, callback);
+    };
+    
+    this.fetchIOU = function(callback) { 
+        var url = constructURL({action: 'iou_get'})
+        request(url, callback);
+    };
+    this.fetchInventoryGet = function(callback) {
+        var url = constructURL({action: 'inventory_get'})
+        request(url, callback);
+    }
+}
