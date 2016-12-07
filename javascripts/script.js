87c063295a7d85267900dc373ffd8914b953a0eb
johanoxenstierna <joox9891@gmail.com>
2016-12-07 12:06:37 +0100

    Update script.js

--- javascripts/script.js
+++ javascripts/script.js
@@ -7,38 +7,8 @@
 // - jQuery document.ready moved to separate file because script.js needs to come before the HTML files calls to the script (e.g. loadUserInfo()), and jQueryScript needs to come after. async / defer didn't work on Opera for some reason
 
 
-//(index) The below 3 functions are used by drag and drop
-function allowDrop(ev) {
-    ev.preventDefault(); 
-}
 
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id); 
-}
-
-function dropcopy(ev)
-                    {
-                    ev.preventDefault();
-                    var data=ev.dataTransfer.getData("Text");
-                    var copyimg = document.createElement("img");
-                    //copyimg.setAttribute("src", "images/hydrangeas.jpg");
-                    copyimg.setAttribute("height", "150");
-                    copyimg.setAttribute("width", "75");
-                    var original = document.getElementById(data);
-                    copyimg.src = original.src;
-                    ev.target.appendChild(copyimg);
-
-                    }
-
-function dropped(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-
-}
-
-
-// ...
+//where is this function used can someone make a comment here? 
 function capitalise(txt) {
 		var arr = txt.split('_'),
 			capTxt = '';
@@ -60,7 +30,7 @@ function capitalise(txt) {
 /* Here starts the API*/
 /**********************/
 
-
+//WHERE IS THIS FUNCTION USED? 
 function usr(responseString){
     var json = JSON.parse(responseString)
     var payload = json.payload
@@ -79,10 +49,6 @@ function usr(responseString){
 
 
 
-
-
-
-
 //(admin) USERALL this function loads all the users into rows and creates an edit button for each user (the button is not connected to the row here but in the jQuery document) 
 function usrall(responseString) {
     var json = JSON.parse(responseString);
@@ -90,7 +56,6 @@ function usrall(responseString) {
     for (var i = 0; i < payload.length; i++) {
 
         var myN = i.toString();
-
         var row = document.getElementById("usersTable").insertRow(i + 1);
 
         row.insertCell();
@@ -106,11 +71,32 @@ function usrall(responseString) {
         document.getElementById("usersTable").rows[i + 1].cells[4].innerHTML = "";
 
         //this creates a button in the rows cell 4 with the index as id
-        var myEditButton = $('<button id =' + i + '>Edit</button>');
+        var myEditButton = $('<button id =' + i + 'editUserButton' + '>Edit</button>');
         $(myEditButton).appendTo(document.getElementById("usersTable").rows[i + 1].cells[4]);
+    }
 
         //The button connection to the row can perhaps be moved here. 
-    }
+    
+	
+    ////TEMP THIS IS A POPUP WINDOW FOR TESTING
+    ////the popup div UNDERSTAND IT
+    //var myTest = $('<p>KKKKK</p>');
+    //var myPopupDiv = $('<div id ="hhh"><p>MYDIIIIV</p></div>');
+
+
+    //var div = document.createElement('div');
+
+    //div.className = 'row';
+
+    //div.innerHTML = '<label>First Name</label>\
+    //                <input type="text" name="name" value="" />\
+    //                <input type="text" name="value" value="" />\
+    //                <label> <input type="checkbox" name="check" value="1" /> Checked? </label>\
+    //                <input type="button" value="Do edits!" onclick="removeRow(this)">';
+
+    ////popup must be present in html
+    //document.getElementById('popup').appendChild(div);
+
 }
 
 //Function determand if connection is correct
@@ -161,98 +147,128 @@ function testadmin(){
 
 
 
-//INVENTORY 
-function allinventory(responseString){
+//(index) INVENTORY 
+function allinventory(responseString) {
     var json = JSON.parse(responseString);
     var payload = json.payload;
-    
-    var beerlist = ["193002","195202","192003","165903","133603",
-                    "1152803","152503","167903","163203","160903",
-                    "1137903","122203","183502","182402","171903",
-                    "667102","207504","223301","721801","651201"];
-    
-     for (var i = 0; i < 4; i++){
-      var row = document.getElementById("drinktable").insertRow(i);
-        for (var k = 0; k<5; k++){
+
+    var beerlist = ["193002", "195202", "192003", "165903", "133603",
+                    "1152803", "152503", "167903", "163203", "160903",
+                    "1137903", "122203", "183502", "182402", "171903",
+                    "667102", "207504", "223301", "721801", "651201"];
+
+    //these two loops create a matrix whose cells contain an img and info about each beer. 
+    //for every row
+    for (var i = 0; i < 4; i++) {
+        var row = document.getElementById("drinktable").insertRow(i);
+
+        //for every cell: 
+        for (var k = 0; k < 5; k++) {
             row.insertCell();
-            
-            //Hämta beer
+
+            //Hämta beer. Laddning fr[n databasen h'nder h''r
             var beerindex = 0;
-            for (beerindex = 0; beerindex < payload.length;beerindex++){
-                if(payload[beerindex].beer_id == beerlist[i*5+k]){
+            for (beerindex = 0; beerindex < payload.length; beerindex++) {
+                if (payload[beerindex].beer_id == beerlist[i * 5 + k]) {
                     break;
                 }
             }
-            
-            
-            
-  // document.getElementById("drinktable").rows[i].cells[k].innerHTML = responseString;
-   // break;
-            
-            
+
+            // document.getElementById("drinktable").rows[i].cells[k].innerHTML = responseString;
+            // break;
+
+            //here is where the HTML is changed to accomodate the beverage: 
             document.getElementById("drinktable").rows[i].cells[k].innerHTML =
-                '<div class="aBeverage"><img id=' + payload[beerindex].beer_id + ' width="70px" height="150px"src=Pictures/' + payload[beerindex].beer_id + '.png draggable="true"ondragstart="drag(event)"><br>' + payload[beerindex].namn + '<br>' + payload[beerindex].namn2 + '<br> Price: ' + payload[beerindex].price + '</div>'
-                
-                
-                //"<img src=" + payload[i*5+k].beer_id + ".png><br>" + //payload[i*5+k].namn + "<br> Price: " + payload[i*5+k].price 
-            	
+            '<div class="aBeverage">' + 
+                '<img id=' + payload[beerindex].beer_id + ' width="70px" height="150px"src=Pictures/' + payload[beerindex].beer_id + '.png draggable="true"ondragstart="drag(event)">' + '<br>' +
+                '<div id="bottleCartInfo">' +
+                    payload[beerindex].namn + '<br>' +
+                    payload[beerindex].namn2 + '<br>' +
+                    'Price: ' + payload[beerindex].price +
+                '</div>'
+            '</div>'
+
+            //"<img src=" + payload[i*5+k].beer_id + ".png><br>" + //payload[i*5+k].namn + "<br> Price: " + payload[i*5+k].price 
+
         }
     }
-    
-    
+
+    //WHERE IS THIS USED? 
     /*for (var i = 0; i < 4; i++){
-      var row = document.getElementById("drinktable").insertRow(i);
+        var row = document.getElementById("drinktable").insertRow(i);
         for (var k = 0; k<5; k++){
             row.insertCell();
             document.getElementById("drinktable").rows[i].cells[k].innerHTML =
                 '<div class="aBeverage"><img id=' + payload[i*5+k].beer_id + ' width="100px" height="100px"src=' + payload[i*5+k].beer_id + '.png draggable="true"ondragstart="drag(event)"><br> Price: ' + payload[i*5+k].price + '</div>'
-                
-                
+                    
+                    
                 //"<img src=" + payload[i*5+k].beer_id + ".png><br>" + //payload[i*5+k].namn + "<br> Price: " + payload[i*5+k].price 
-            	
+                    
         }
     */
 }
 
 
+//(view history?) PURCHASES USER 
 
-//INVENTORY
-
-//PURCHASES USER 
-
-function purchases(responseString){
+function purchases(responseString) {
     var json = JSON.parse(responseString);
     var payload = json.payload;
-    for (var i = 0; i < payload.length; i++){
-      var row = document.getElementById("drinktable").insertRow(i+1);
+    for (var i = 0; i < payload.length; i++) {
+        var row = document.getElementById("drinktable").insertRow(i + 1);
         row.insertCell();
         row.insertCell();
         row.insertCell();
-      document.getElementById("drinktable").rows[i+1].cells[0].innerHTML = payload[i].namn + ' ' + payload[i].namn2
-      document.getElementById("drinktable").rows[i+1].cells[1].innerHTML = payload[i].price
-      document.getElementById("drinktable").rows[i+1].cells[2].innerHTML = payload[i].timestamp
+        document.getElementById("drinktable").rows[i + 1].cells[0].innerHTML = payload[i].namn + ' ' + payload[i].namn2
+        document.getElementById("drinktable").rows[i + 1].cells[1].innerHTML = payload[i].price
+        document.getElementById("drinktable").rows[i + 1].cells[2].innerHTML = payload[i].timestamp
+    }
+}
+
+//(admin) purchasesAllAdmin
+function purchasesGetAllAdmin(responseString) {
+    var json = JSON.parse(responseString);
+    var payload = json.payload;
+    for (var i = 0; i < payload.length; i++) {
+
+        var myN = i.toString();
+        var row = document.getElementById("usersTable").insertRow(i + 1);
+
+        row.insertCell();
+        row.insertCell();
+        row.insertCell();
+        row.insertCell();
+        row.insertCell();
+
+        document.getElementById("usersTable").rows[i + 1].cells[0].innerHTML = payload[i].first_name;
+        document.getElementById("usersTable").rows[i + 1].cells[1].innerHTML = payload[i].last_name;
+        document.getElementById("usersTable").rows[i + 1].cells[2].innerHTML = payload[i].email;
+        document.getElementById("usersTable").rows[i + 1].cells[3].innerHTML = payload[i].phone;
+        document.getElementById("usersTable").rows[i + 1].cells[4].innerHTML = "";
+
+        //this creates a button in the rows cell 4 with the index as id
+        var myEditButton = $('<button id =' + i + 'editUserButton' + '>Edit</button>');
+        $(myEditButton).appendTo(document.getElementById("usersTable").rows[i + 1].cells[4]);
     }
 }
 
 
-
+//CAN THIS BE REMOVED? 
 //PURCHASES USER 
-
-function fn(){
+function fn() {
     var api = new APIConnect();
     //btn = document.getElementById('yourbuttonID');
 
-    //api.setUser('benfau', 'benfau');
+    api.setUser('benfau', 'benfau');
     //btn.addEventListener('click', function() { loadUsers(api) });
-   
-    //api.fetchIOU(usr);
-        
+
+    //api.fetchIOU(usr);   
 }
 
 
-
+////WHERE IS THIS FUNCTION USED? 
 function docLoaded(fn) {
-   
+
     if (document.readyState !== 'loading') {
         fn();
     } else {
@@ -266,29 +282,39 @@ function docLoaded(fn) {
 
 //HTMLs function calls ---------------------------------------------------------
 
-//perhaps only one "var api = new APIConnect();" is necessary
+//perhaps only one "var api = new APIConnect();" is necessary? 
 
 //Index: to load the bevarages
 function loadInventory() {
     var api = new APIConnect();
+    api.setUser('jora', 'benfau');
     api.fetchInventoryGet(allinventory);
 }
 
-
-//History: This function is loaded from history page
-function loadpurchasesget() {
-    var api = new APIConnect();
-    api.fetchPurchasesGet(purchases);
-} 
-
-//Admin: This function is loaded from admin page
+//Admin->UserInfo
 function loadUserInfo() {
     var api = new APIConnect();
+    api.setUser('jorass', 'jorass');
     api.fetchUsers(usrall);
 }
 
-
-//... probably the same as above
-function loadUsers(api) {
-    api.fetchUsers(usrall)
+//History: This function is loaded from history page
+function loadPurchasesGet() {
+    var api = new APIConnect();
+    api.setUser('jorass', 'jorass');
+    api.fetchPurchasesGet(purchases);
 }
+
+//Admin: this loads purchases_get_all for the admin
+function loadPurchasesGetAllAdmin() {
+    var api = new APIConnect();
+    api.setUser('jorass', 'jorass');
+    api.fetchPurchasesGetAllAdmin(purchasesGetAllAdmin);
+
+}
+
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
