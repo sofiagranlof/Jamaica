085802cf64678c150c6969357bb49df12e886f2b
AngelicaElvin <angelica.elvin@gmail.com>
2017-01-09 14:20:13 +0100

    Update jQueryScript.js

--- javascripts/jQueryScript.js
+++ javascripts/jQueryScript.js
@@ -4,23 +4,6 @@
 
 $(document).ready(function () {
 
-    //This is testing code to make it easier to see that jQuery works
-    //$("#top_header").hide(200).slideDown(300); 
-    //$('li').append("");
-    //$(document).on('click', '.panel-button', function () { //'.panel-button'
-    //    console.log("panel_button");
-    //    //$('#myDiv').toggle();
-    //    var panelId = $(this).attr('data-panelid');
-
-    //    console.log("panelId: " + panelId);
-    //    $('#' + panelId).toggle();
-
-    //    $("afadsfadsf").appendTo(document.getElementById("usersTable").rows[1].cells[3]);
-
-    //    var uuu = $('<p>hujhjh</p>');
-    //    $(uuu).appendTo(document.getElementById("usersTable").rows[1].cells[4]); 
-
-    //});
 
     //(index) The below 3 functions are used by drag and drop
   
@@ -34,13 +17,11 @@ $(document).ready(function () {
             var rowNumberString = $(this).attr('id');
             var rowNumberInt = parseInt(rowNumberString) + 2;
             
-            //alert("The user " + firstName + " " + lastName + " will be changed using a jQuery form");
-
             //MODAL WINDOW (based on w3Schools template at http://www.w3schools.com/howto/howto_css_modals.asp -----------------------------------------------------------
             //Now that the row index has been got a modal window can be created with this info. 
             var modal = document.getElementById('myModal');
 
-            //console.log("rowNumberInt: " + rowNumberInt); 
+          
 
             // Get the button that opens the modal
             rowNumberInt -= 1; 
@@ -76,7 +57,6 @@ $(document).ready(function () {
             var phone = document.getElementById("usersTable").rows[rowNumberInt].cells[3].innerHTML;
 
 
-            // var spawnModalHTML = $('<button id = "editUserButton">Edit</button>');
             var spawnModalHTML = $(
                 '<div id="jQueryForm">' +
                     '<h3>Edit ' + firstName + ' ' + lastName + '</h3>' +
@@ -101,7 +81,6 @@ $(document).ready(function () {
             var balance = document.getElementById("usersTable").rows[rowNumberInt].cells[3].innerHTML;
 
 
-            // var spawnModalHTML = $('<button id = "editUserButton">Edit</button>');
             var spawnModalHTML = $(
                 '<div id="jQueryForm">' +
                     '<h3>Edit the Balace of ' + firstname + ' ' + lastname + '</h3>' +
@@ -120,7 +99,6 @@ $(document).ready(function () {
             var price = document.getElementById("usersTable").rows[rowNumberInt].cells[4].innerHTML;
 
 
-            // var spawnModalHTML = $('<button id = "editUserButton">Edit</button>');
             var spawnModalHTML = $(
                 '<div id="jQueryForm">' +
                     '<h3>Edit ' + name  + '</h3>' +
@@ -143,7 +121,7 @@ $(document).ready(function () {
   $(spawnModalHTML).appendTo(document.getElementById("myForm"));
 
 
-        }); //.on('click', '#' + j + 'editUserButton'
+        }); 
     }//for loop
 
 
@@ -151,91 +129,6 @@ $(document).ready(function () {
 
     //for each of the beers in the index page
     for (var i = 0; i < 1; i++) { //perhaps payload.length
-
-       
-
-
-        ////DRAG&DROP --------------------------------------------
-        //$(document).on("drag", function (event) {
-        //    //event.dataTransfer.setData("text", event.target.id);
-        //});
-
-        //$(document).on("dragover", function (event) {
-        //    //event.preventDefault();
-        //    //event.stopPropagation();
-        //    //$(this).addClass('dragging');
-        //});
-
-        ////when a dragged element or text selection leaves a valid drop target.
-        //$(document).on("dragleave", function (event) {
-        //    //event.preventDefault();
-        //    // event.stopPropagation();
-        //    // $(this).removeClass('dragging');
-        //});
-
-
-        
-
-        //$(document).on("drop", function (event) {
-
-
-        //    var rowNumberString = $(this).attr('id');
-        //    var rowNumberInt = parseInt(rowNumberString) + 1;
-
-
-        //    //create empty div inside cart where the dragged bottle can be put. 
-        //    var newHTML = $('<li>aadsfdsfa</li>');
-        //    // $(newHTML).appendTo(document.getElementsByClassName("cart"));
-
-        //    //collect a copy of the bottle info
-        //    var nodeCopy = document.getElementById("bottleCartInfoFromRow0Cell0").cloneNode(true);
-        //    nodeCopy.id = "newId";
-
-
-        //    $(".cart").append(newHTML);
-
-            
-
-
-        //    var fff = $("#193002").attr('id');
-        //    console.log(fff); 
-
-           
-
-        //    //$("#newId").css("background", "red");
-
-            
-            
-
-        //    //get all the HTML info to build a new HTML
-
-
-        //    //var myCell = document.getElementById("drinktable").rows[0].cells[4].innerHTML;
-        //    //$(".cart").append(myCell);
-
-
-
-        //    //ev.preventDefault();
-        //    var data = event.dataTransfer.getData("Text");
-        //    var copyimg = document.createElement("img");
-        //    copyimg.setAttribute("src", "images/hydrangeas.jpg");
-        //    copyimg.setAttribute("height", "150");
-        //    copyimg.setAttribute("width", "75");
-        //    var original = document.getElementById(data);
-        //    copyimg.src = original.src;
-        //    ev.target.appendChild(copyimg);
-
-
-        //});
-
-
-
-        //$('#test').draggable({
-        //    start: function (event, ui) {
-        //        console.log(event);
-        //        console.log(ui);
-        //    }
-        //});
 
     }//for loop
 
@@ -266,44 +159,3 @@ $("nav select").change(function() {
   window.location = $(this).find("option:selected").val();
 }); //End of drop-down menu
 
-
-
-
-
-//function drag(ev) {
-//    ev.dataTransfer.setData("text", ev.target.id);
-//}
-
-//function dropcopy(ev) {
-//    ev.preventDefault();
-//    var data = ev.dataTransfer.getData("Text");
-//    var copyimg = document.createElement("img");
-//    //copyimg.setAttribute("src", "images/hydrangeas.jpg");
-//    copyimg.setAttribute("height", "150");
-//    copyimg.setAttribute("width", "75");
-//    var original = document.getElementById(data);
-//    copyimg.src = original.src;
-//    ev.target.appendChild(copyimg);
-
-
-//    //local variables used to generate HTML below
-//    //var price =
-
-//    // document.getElementById("drinktable").rows[i].cells[k].innerHTML =
-//    //            '<div class="aBeverage"><img id=' + payload[beerindex].beer_id +
-//    //                ' width="70px" height="150px"src=Pictures/' + payload[beerindex].beer_id +
-//    //                '.png draggable="true"ondragstart="drag(event)"><br>' + payload[beerindex].namn +
-//    //                '<br>' + payload[beerindex].namn2 + '<br> Price: ' + payload[beerindex].price +
-//    //            '</div>'
-
-
-//    var spawnCartHTML = $('<li><h3>adsfdsadsa</h3></li>');
-//    $(spawnCartHTML).appendTo(document.getElementsByClassName("cart"));
-//}
-
-//function dropped(ev) {
-//    ev.preventDefault();
-//    var data = ev.dataTransfer.getData("text");
-//    ev.target.appendChild(document.getElementById(data));
-
-//}
